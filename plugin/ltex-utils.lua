b38c3dafc72074c3888d1676b25e7aa2edfe8bc0
Johannes Hofscheier <johanneshofscheier@MacBook-Pro-von-Johannes-2.local>
2023-06-16 17:07:26 +0100

    feat: add user commands and autocompletion
    - Implement user commands to streamline interaction with the ltex-utils
      plugin.
    - Add autocompletion functionality to enhance usability of user commands.

---
+++ plugin/ltex-utils.lua
@@ -0,0 +1,46 @@
+if vim.g.loaded_ltex_utils == 1 then
+  return
+end
+vim.g.loaded_ltex_utils = 1
+
+local ltex_lsp = require("ltex-utils.ltex_lsp")
+local builtin = require("ltex-utils.builtin")
+
+vim.api.nvim_create_user_command("LTeXUtils", function(opts)
+	if #opts.fargs == 1 then
+		builtin[opts.fargs[1]]()
+	end
+--  require("telescope.command").load_command(unpack(opts.fargs))
+end, {
+	nargs = "*",
+	complete = function(_, line)
+		local builtin_list = vim.tbl_keys(require("ltex-utils.builtin"))
+		local l = vim.split(line, "%s+")
+		local n = #l - 2
+
+		if n == 0 then
+			table.sort(builtin_list)
+
+			return vim.tbl_filter(function(val)
+				return vim.startswith(val, l[2])
+			end, builtin_list)
+		end
+
+		if n == 1 and l[2] == "modify_dict" then
+			local client = ltex_lsp.get_ltex()
+			if not client then return end
+
+			local settings, err = ltex_lsp.get_settings(client, "dictionary")
+			if not settings then
+				return false, err
+			end
+
+			local dicts = vim.tbl_keys(settings)
+			table.sort(dicts)
+
+			return vim.tbl_filter(function(val)
+				return vim.startswith(val, l[3])
+			end, dicts)
+		end
+	end,
+})
