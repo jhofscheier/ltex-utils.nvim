ac607e56911a0fd8ecc0b0db1e7170d44d30bd99
Johannes Hofscheier <johanneshofscheier@MBPvonJohannes2.lan>
2023-07-09 10:25:13 +0100

    feat: collect diagnostics from LTeX LSP server

--- lua/ltex-utils/ltex_lsp.lua
+++ lua/ltex-utils/ltex_lsp.lua
@@ -1,5 +1,7 @@
 local M = {}
 
+local Config = require("ltex-utils.config")
+
 --- Returns the first active LTeX LSP client attached to a buffer.
 --
 -- @param bufnr Buffer number (optional). If not provided, uses current buffer.
@@ -77,6 +79,123 @@ function M.get_settings_for_lang(client, setting_cfg, lang)
 	end
 
 	return lang_rules
+end
+
+function M.get_server_rules(bufnr)
+end
+
+
+---Returns a handler for `textDocument/publishDiagnostics` notifications.
+---The handler collects diagnostics data using a timer to batch process it.
+---@param accumulated_diagnostics table Table to store diagnostics data
+---@param callback function Function called after all data is collected
+---@return function # "textDocument/publishDiagnostics"-notification handler
+function M.on_publish_diags(accumulated_diagnostics, callback)
+	local diag_timer = nil
+
+	return function (
+		err,
+		result,
+		ctx
+	)
+		if err then
+			vim.notify("Error on 'textDocument/publishDiagnostics'" ..
+				vim.inspect(err), vim.log.levels.ERROR)
+				return
+		end
+
+		-- The original diagnostics handler
+		vim.lsp.with(
+			vim.lsp.diagnostic.on_publish_diagnostics, {
+				-- custom settings here
+			}
+		)(err, result, ctx)
+
+		-- Add new diagnostics to our table
+		table.insert(accumulated_diagnostics, result)
+
+		-- If there's an existing timer, stop it
+		if diag_timer then
+			diag_timer:stop()
+			diag_timer:close()
+		end
+
+		-- Start a new timer
+		diag_timer = vim.loop.new_timer()
+
+		-- Schedule the timer to call our function after debounce_time_ms
+		diag_timer:start(
+			Config.diagnostics.debounce_time_ms,
+			0,
+			vim.schedule_wrap(function()
+				callback()
+				-- Clear the diagnostics and the timer
+				diag_timer:stop()
+				diag_timer:close()
+				diag_timer = nil
+			end)
+		)
+	end
+end
+
+--- Requests code actions for given diagnostics,
+-- processes them, and triggers a final callback.
+--
+-- @param client The LSP client.
+-- @param bufnr The buffer number.
+-- @param diags List of diagnostics.
+-- @param process_action Function to process each code action.
+-- @param final_callback Function called when all requests have been processed.
+function M.collect_diagnostics_actions(
+	client,
+	bufnr,
+	diags,
+	process_action,
+	final_callback
+)
+	local pending_reqs = 0
+	for _, chunk in ipairs(diags) do
+		pending_reqs = pending_reqs + #chunk.diagnostics
+	end
+
+	for _, chunk in ipairs(diags) do
+		local uri = chunk.uri
+		for _, diag in ipairs(chunk.diagnostics) do
+			 local params = {
+				textDocument = { uri = uri },
+				range = diag.range,
+				context = { diagnostics = diag },
+			}
+
+			client.request(
+				'textDocument/codeAction',
+				params,
+				function(err, actions)
+					if err then
+						vim.notify("Error getting code action: " ..
+							vim.inspect(err), vim.log.levels.ERROR)
+						return
+					end
+					process_action(err, actions)
+
+					pending_reqs = pending_reqs - 1
+
+					-- have all request be collected?
+					if pending_reqs == 0 then
+						final_callback()
+
+						client.notify(
+							"workspace/didChangeConfiguration",
+							client.config.settings
+						)
+					end
+
+					return true
+				end,
+				bufnr
+			)
+		end
+	end
 end
 
 return M
