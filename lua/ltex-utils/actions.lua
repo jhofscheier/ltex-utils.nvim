f9d3a27559851a5a64a8767669cebc319367c874
Johannes Hofscheier <johanneshofscheier@MBPvonJohannes2.lan>
2023-07-09 10:21:09 +0100

    fix: writing dictionaries to file

--- lua/ltex-utils/actions.lua
+++ lua/ltex-utils/actions.lua
@@ -1,20 +1,15 @@
 local M = {}
 
+local Config = require("ltex-utils.config")
 local ltex_lsp = require("ltex-utils.ltex_lsp")
 local settings_io = require("ltex-utils.settings_io")
 local table_utils = require("ltex-utils.table_utils")
 
---- Configuration for the plugin.
--- @module ltex-utils.actions
-
--- @field dict_path Path to the directory where dictionaries are stored.
--- Defaults to the Neovim cache directory.
-M.dict_path = vim.api.nvim_call_function("stdpath", {"cache"}) .. "/ltex/"
-
-
--- Creates a new handler for code actions that updates the LSP server settings.
--- Takes two strings, 'cmd_cfg' and 'setting_cfg', corresponding to the fields
--- in the involved tables.
+---Generates a code action handler updating LSP server settings with 'cmd_cfg'
+---and 'setting_cfg' fields.
+---@param cmd_cfg string The key for the code action command
+---@param setting_cfg string The key of the server settings
+---@return function () # The code action handler
 function M.new_handler(cmd_cfg, setting_cfg)
 	return function (command)
 		local client = ltex_lsp.get_ltex()
@@ -23,11 +18,10 @@ function M.new_handler(cmd_cfg, setting_cfg)
 
 		local settings = ltex_lsp.get_settings_or_init(client, setting_cfg)
 
-		for lang, rule in pairs(command.arguments[1][cmd_cfg]) do
-			table_utils.extend_or_init(settings[setting_cfg], lang, rule)
+		for lang, rules in pairs(command.arguments[1][cmd_cfg]) do
+			table_utils.extend_or_init(settings[setting_cfg], lang, rules)
 		end
 
-		-- client.config.settings.ltex = settings
 		client.notify(
 			"workspace/didChangeConfiguration",
 			client.config.settings
@@ -50,11 +44,13 @@ end
 	Usage example:
 		M.write_ltex_to_file()
 --]]
-function M.write_ltex_to_file()
+---comment
+---@param cached_dict_changes any
+function M.write_ltex_to_file(cached_dict_changes)
 	local client = ltex_lsp.get_ltex()
 	-- if no active ltex client abort
 	if not client then
-		print("No active ltex client found")
+		vim.notify("No active ltex client found", vim.log.levels.ERROR)
 		return
 	end
 
@@ -65,29 +61,24 @@ function M.write_ltex_to_file()
 	local langs
 	-- save dictionaries; update them if necessary
 	if settings.dictionary then
-		settings_io.ensure_folder_exists(M.dict_path)
+		settings_io.ensure_folder_exists(Config.dict_path)
 		langs = settings_io.update_dictionary_files(
-			M.dict_path,
-			settings.dictionary
+			Config.dict_path,
+			settings.dictionary,
+			cached_dict_changes
 		)
 	end
 
 	local settings_to_save = {}
 
-	if settings.hiddenFalsePositives then
-		settings_to_save.hiddenFalsePositives = settings.hiddenFalsePositives
-		langs = table_utils.merge_lists_unique(
-			langs,
-			vim.tbl_keys(settings.hiddenFalsePositives)
-		)
-	end
-
-	if settings.disabledRules then
-		settings_to_save.disabledRules = settings.disabledRules
-		langs = table_utils.merge_lists_unique(
-			langs,
-			vim.tbl_keys(settings.disabledRules)
-		)
+	for _, settings_cfg in ipairs({ "hiddenFalsePositives", "disabledRules" }) do
+		if settings[settings_cfg] then
+			settings_to_save[settings_cfg] = settings[settings_cfg]
+			langs = table_utils.merge_lists_unique(
+				langs,
+				vim.tbl_keys(settings[settings_cfg])
+			)
+		end
 	end
 
 	settings_to_save.langs = langs or nil
@@ -145,7 +136,7 @@ function M.load_ltex_from_file()
 		end
 		-- read the required dictionaries
 		settings_io.load_dictionaries(
-			M.dict_path,
+			Config.dict_path,
 			saved_settings.langs,
 			client_settings.dictionary
 		)
